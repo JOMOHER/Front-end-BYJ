4daf3be66b3962114273a6a568b0ef000308c7a5
Jordi <jordi.moher@gmail.com>
2025-06-13 23:13:27 -0600

    Cambios

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head';
-import Countdown from '../components/Countdown';
+import Head from "next/head";
+import Countdown from "../components/Countdown";
 
 export default function Home() {
   return (
@@ -7,88 +7,139 @@ export default function Home() {
       <Head>
         <title>La vida bonita</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;600;700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Pinyon+Script&display=swap" rel="stylesheet"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Alex+Brush&family=Montserrat:wght@500&family=Playfair+Display&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Pinyon+Script&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Alex+Brush&family=Montserrat:wght@500&family=Playfair+Display&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <header className="hero">
-        <h1>Bren y Jordi</h1>
-        <p>Tenemos el gusto de invitarlos a nuestro enlace matrimonial, con la bendicion de DIOS, nuestros padres, hermanos y seres queridos.</p>
-         <section className="padres">
-        <div className="padres-container">
-         <div class="lado">
-        <p>PEDRO MORENO SALDAÑA</p>
-        <p>BLANCA ESTELA HERNANDEZ SANCHEZ</p>
-        </div>
-        <div class="linea-vertical"></div>
-        <div class="lado">
-        <p>ERASMO BRITO INIESTA</p>
-        <p>ROCISELA PLASCENCIA HERNANDEZ</p>
-        </div>
-        </div>
+        <h1>Brenda y Jordi</h1>
+        <p>
+          Tenemos el gusto de invitarlos a nuestro enlace matrimonial, con la
+          bendicion de DIOS, nuestros padres, hermanos y seres queridos.
+        </p>
+        <section className="padres">
+          <div className="padres-container">
+            <div class="lado">
+              <p>PEDRO MORENO SALDAÑA</p>
+              <p>BLANCA ESTELA HERNANDEZ SANCHEZ</p>
+            </div>
+            <div class="linea-vertical"></div>
+            <div class="lado">
+              <p>ERASMO BRITO INIESTA</p>
+              <p>ROCISELA PLASCENCIA HERNANDEZ</p>
+            </div>
+          </div>
         </section>
         <p className="date">13 de Diciembre de 2025</p>
       </header>
 
       <section className="fotos">
-        <h1>Así que ya no son dos, sino uno solo. Por tanto, lo que Dios ha unido, que no lo separe el hombre.</h1>
+        <h1>
+          Así que ya no son dos, sino uno solo. Por tanto, lo que Dios ha unido,
+          que no lo separe el hombre.
+        </h1>
         <div className="foto-seccion">
           <img src="/img/PedidaBN.jpeg" alt="AMOR" />
         </div>
       </section>
 
       <section className="detalles">
-        <h2>
-          Detalles del Evento
-        </h2>
-        <p><strong>Fecha:</strong> 13 de Diciembre de 2025</p>
-        <p><strong>Lugar:</strong> Jardín Magnolia, Jiutepec Morelos</p>
-        <p><strong>Recepción:</strong> 4:00 PM</p>
+        <h2>Detalles del Evento</h2>
+        <p>
+          <strong>Fecha:</strong> 13 de Diciembre de 2025
+        </p>
+        <p>
+          <strong>Lugar:</strong> Jardín Magnolia, Jiutepec Morelos
+        </p>
+        <p>
+          <strong>Recepción:</strong> 4:00 PM
+        </p>
         <div className="boton-con-icono">
-    <img src="/img/location.svg" alt="Ubicación" className="icono" />
-        <a href="https://maps.app.goo.gl/219x1PZCheMR7wWe6?g_st=com.google.maps.preview.copy" target="_blank" rel="noopener noreferrer">
-        Cómo llegar
-        </a>
-      </div>
-      
+          <img src="/img/location.svg" alt="Ubicación" className="icono" />
+          <a
+            href="https://maps.app.goo.gl/219x1PZCheMR7wWe6?g_st=com.google.maps.preview.copy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Cómo llegar
+          </a>
+        </div>
+
         <div className="foto-seccion">
           <img src="/img/detalle-evento.jpeg" alt="Nos comprometimos" />
         </div>
       </section>
 
- <section className="detalles vestimenta">
-  <div className="icon-text-container">
-    <img src="/img/suit1.svg" alt="traje" className="iconv" />
-    
-    <p className="codigo-vestimenta"><strong> <h2>Código de vestimenta</h2> </strong> <p>Formal / Etiqueta</p> </p>
+      <section className="detalles vestimenta">
+        <div className="icon-text-container">
+          <img src="/img/suit1.svg" alt="traje" className="iconv" />
 
-    <img src="/img/dress.svg" alt="vestido" className="iconv" />
-  </div>
+          <p className="codigo-vestimenta">
+            {" "}
+            <h2>
+              <strong> Código de vestimenta </strong>{" "}
+            </h2>{" "}
+            <p>Formal / Etiqueta</p>{" "}
+          </p>
 
-  {/* Aquí va el texto que quieres mostrar */}
-  <h3 className="subtitulo-paleta">Paleta de colores sugerida</h3>
+          <img src="/img/dress.svg" alt="vestido" className="iconv" />
+        </div>
 
-  <div style={{ textAlign: 'center' }}>
-    <img src="/img/paleta3.png" alt="Paleta de colores" className="imagen-paleta" />
-  </div>
-</section>
+        {/* Aquí va el texto que quieres mostrar */}
+        <h3 className="subtitulo-paleta">Paleta de colores sugerida</h3>
+
+        <div style={{ textAlign: "center" }}>
+          <img
+            src="/img/paleta3.png"
+            alt="Paleta de colores"
+            className="imagen-paleta"
+          />
+        </div>
+      </section>
 
       <div className="foto-seccion">
         <img src="/img/VistaBN.jpeg" alt="Te esperamos" />
       </div>
 
-        <section className="detalles">
-        <h2>
-          Asistencia / Mesa de regalos
-        </h2>  
-       <div className="boton-con-icono">
-    <img src="/img/confirm.svg" alt="Confirmacion" className="icono" />
-        <a href="https://maps.app.goo.gl/219x1PZCheMR7wWe6?g_st=com.google.maps.preview.copy" target="_blank" rel="noopener noreferrer">
-          Confirmar asistencia
-        </a>
-      </div>
+      <section className="detalles">
+        <h2>Asistencia / Mesa de regalos</h2>
+        <div className="button-container">
+          <div className="boton-con-icono">
+          <img src="/img/confirm.svg" alt="Confirmacion" className="icono" />
+          <a
+            href="https://maps.app.goo.gl/219x1PZCheMR7wWe6?g_st=com.google.maps.preview.copy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Confirmar asistencia
+          </a>
+        </div>
+          <div className="boton-con-icono">
+          <img src="/img/gift.svg" alt="Confirmacion" className="icono" />
+          <a
+            href="https://maps.app.goo.gl/219x1PZCheMR7wWe6?g_st=com.google.maps.preview.copy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Mesa de regalos
+          </a>
+        </div>
+        </div>
+        
       </section>
       <section>
         <h1>FALTAN</h1>
